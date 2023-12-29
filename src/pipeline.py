fa6f5ab3ae0a44e8f23649cec0f6a06e9e717be1
Shelby Thomas <shelbyt@ucsd.edu>
2023-12-29 14:16:50 -0800

    small updates, not working still

--- src/pipeline.py
+++ src/pipeline.py
@@ -4,32 +4,35 @@
 # 2. Convert each sentence to an embedding vector (1536) (OAI ADA-EMBED API)
 # 3. Batch Upsert the embedding vector into Pinecone using the index, vector, and column info as metadata (Pinecone API)
 
+import os
 import pinecone
 import csv
-import openai
+from openai import OpenAI
+from openai import AsyncOpenAI
 import requests
-import os
 from dotenv import load_dotenv
 import argparse
 
 load_dotenv()
+client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
 # Initialize APIs
-openai.api_key = os.getenv('OPENAI_API_KEY')
 pinecone.init(api_key=os.getenv('PINECONE_API_KEY'), environment='us-west1-gcp')
 # Function to generate sentence using GPT-4
 def generate_sentence(input_row, columns=None):
     prompt = f"convert this row from a dataset into a 100 word concise but descriptive paragraph with all the  technical specs that I can convert into an embedding. here are the columns for the dataset ensure information from each column is included: {columns} -> {input_row}"
-    response = openai.Completion.create(
-        model="gpt-3.5-turbo",
-        prompt=prompt,
-        max_tokens=3000
-    )
+    response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+        messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": prompt},
+    ],
+    max_tokens=3000)
     return response.choices[0].text.strip()
 
 # Function to convert sentence to embedding
 def convert_to_embedding(sentence):
-    response = openai.embeddings.create(
+    response = client.embeddings.create(
         model="text-embedding-ada-002",
         input=sentence
     ) 
@@ -46,11 +49,14 @@ def main():
     parser.add_argument('csvfile', type=str, help='The CSV file to process')
     args = parser.parse_args()
 
-    with open('your_file.csv', newline='') as csvfile:
+    with open(args.csvfile, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
+        columns = " ".join(reader.fieldnames)  # Get headers as a space-separated sentence
         for row in reader:
-            # Assuming 'text' is the column you want to process
-            generated_sentence = generate_sentence(row['text'])
+            row_str = " ".join([str(val) for val in row.values()])
+            generated_sentence = generate_sentence(row_str, columns)
+            print (generated_sentence)
+            exit(1)
             embedding_vector = convert_to_embedding(generated_sentence)
             metadata = {key: row[key] for key in row if key != 'text'}
             print (metadata)
